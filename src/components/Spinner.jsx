78400903946c7bbab773a4a1c57ce4418674dd5a
aziz <azizsellal.dz@gmail.com>
2024-10-04 22:55:27 +0100

    delete all files

--- src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-//just un add-on
-function Spinner() {
-  return (
-    <div className='animated-ping w-16 h-16 m-8 rounded-full bg-sk-600'></div>
-  )
-}
-
-export default Spinner
